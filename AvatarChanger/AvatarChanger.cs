c7315495dde452523f1750ee815884db52601256
Fuvii <36974246+FuviiPeshu@users.noreply.github.com>
2025-05-23 08:32:34 +0200

    Add VRCOSC modules base files

---
+++ AvatarChanger/AvatarChanger.cs
@@ -0,0 +1,24 @@
+﻿using VRCOSC.App.SDK.Modules;
+
+namespace FuviiOSC.AvatarChanger;
+
+[ModuleTitle("Avatar Changer")]
+[ModuleDescription("Handles avatar change via OSC")]
+[ModuleType(ModuleType.Generic)]
+public class AvatarChangerModule : Module
+{
+    protected override void OnPreLoad()
+    {
+        CreateState(AvatarChangerState.Default, "Default");
+    }
+
+    private enum AvatarChangerSetting
+    {
+        AvatarChangerTriggerInstances
+    }
+
+    private enum AvatarChangerState
+    {
+        Default
+    }
+}
