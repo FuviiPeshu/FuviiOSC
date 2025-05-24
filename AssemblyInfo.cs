de3fe8873bb84ce4ec8a073afa06a1867ac565bf
Fuvii <36974246+FuviiPeshu@users.noreply.github.com>
2025-05-24 18:43:50 +0200

    Fix build after name change

---
+++ AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Fuvii's Modules")]
+[assembly: AssemblyDescription("All of Fuvii's VRCOSC modules")]
+[assembly: SuppressMessage("Platform Compatibility", "CA1416:This call site is reachable on all platforms")]
